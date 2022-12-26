96397269b3bdce681553d91f09690a0d087494c9
Morteza Poussaneh <49806042+MortezaPoussaneh@users.noreply.github.com>
2022-12-26 16:32:20 +0100

    Use installed packages

--- Console.php
+++ Console.php
@@ -3,8 +3,9 @@
 use Saeghe\ChuckNorris\Response;
 use Saeghe\ChuckNorris\Request;
 use function Saeghe\ChuckNorris\Api\get;
+use function Saeghe\Cli\IO\Write\line;
 
 $request = new Request();
 $response = new Response(get($request->url()));
 
-echo $response->joke();
+line($response->joke());
